195d1587cfb43ff433be748899ab8016d6bf1ff3
hQQn <dahunee37@gmail.com>
2025-04-13 19:48:00 +0900

    chore: add prettierrrc

--- apps/web/src/components/ui/Heading/Heading.tsx
+++ apps/web/src/components/ui/Heading/Heading.tsx
@@ -6,13 +6,16 @@ import { cn } from "@/common/utils";
  * 헤딩 컴포넌트
  */
 export const Heading1 = memo(
-  forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(
-    ({ className, ...props }, ref) => {
-      return (
-        <BaseHeading ref={ref} as="h1" className={cn(className)} {...props} />
-      );
-    }
-  )
+	forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(({ className, ...props }, ref) => {
+		return (
+			<BaseHeading
+				ref={ref}
+				as="h1"
+				className={cn(className)}
+				{...props}
+			/>
+		);
+	}),
 );
 
 Heading1.displayName = "Heading1";
@@ -21,13 +24,16 @@ Heading1.displayName = "Heading1";
  * 헤딩 2 컴포넌트
  */
 export const Heading2 = memo(
-  forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(
-    ({ className, ...props }, ref) => {
-      return (
-        <BaseHeading ref={ref} as="h2" className={cn(className)} {...props} />
-      );
-    }
-  )
+	forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(({ className, ...props }, ref) => {
+		return (
+			<BaseHeading
+				ref={ref}
+				as="h2"
+				className={cn(className)}
+				{...props}
+			/>
+		);
+	}),
 );
 
 Heading2.displayName = "Heading2";
@@ -36,13 +42,16 @@ Heading2.displayName = "Heading2";
  * 헤딩 3 컴포넌트
  */
 export const Heading3 = memo(
-  forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(
-    ({ className, ...props }, ref) => {
-      return (
-        <BaseHeading ref={ref} as="h3" className={cn(className)} {...props} />
-      );
-    }
-  )
+	forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(({ className, ...props }, ref) => {
+		return (
+			<BaseHeading
+				ref={ref}
+				as="h3"
+				className={cn(className)}
+				{...props}
+			/>
+		);
+	}),
 );
 
 Heading3.displayName = "Heading3";
@@ -51,13 +60,16 @@ Heading3.displayName = "Heading3";
  * 헤딩 4 컴포넌트
  */
 export const Heading4 = memo(
-  forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(
-    ({ className, ...props }, ref) => {
-      return (
-        <BaseHeading ref={ref} as="h4" className={cn(className)} {...props} />
-      );
-    }
-  )
+	forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(({ className, ...props }, ref) => {
+		return (
+			<BaseHeading
+				ref={ref}
+				as="h4"
+				className={cn(className)}
+				{...props}
+			/>
+		);
+	}),
 );
 
 Heading4.displayName = "Heading4";
@@ -66,13 +78,16 @@ Heading4.displayName = "Heading4";
  * 헤딩 5 컴포넌트
  */
 export const Heading5 = memo(
-  forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(
-    ({ className, ...props }, ref) => {
-      return (
-        <BaseHeading ref={ref} as="h5" className={cn(className)} {...props} />
-      );
-    }
-  )
+	forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(({ className, ...props }, ref) => {
+		return (
+			<BaseHeading
+				ref={ref}
+				as="h5"
+				className={cn(className)}
+				{...props}
+			/>
+		);
+	}),
 );
 
 Heading5.displayName = "Heading5";
@@ -81,13 +96,16 @@ Heading5.displayName = "Heading5";
  * 헤딩 6 컴포넌트
  */
 export const Heading6 = memo(
-  forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(
-    ({ className, ...props }, ref) => {
-      return (
-        <BaseHeading ref={ref} as="h6" className={cn(className)} {...props} />
-      );
-    }
-  )
+	forwardRef<HTMLHeadingElement, Omit<BaseHeadingProps, "as">>(({ className, ...props }, ref) => {
+		return (
+			<BaseHeading
+				ref={ref}
+				as="h6"
+				className={cn(className)}
+				{...props}
+			/>
+		);
+	}),
 );
 
 Heading6.displayName = "Heading6";
