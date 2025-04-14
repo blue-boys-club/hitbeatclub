b93c26620cd07af31d4da075506c99b7f3674628
WonJung Kim <git@won-jung.kim>
2025-04-14 16:17:46 +0900

    fix: wrong configured turbo and prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+const config = {
+	// eslint-disable-next-line @typescript-eslint/no-require-imports, no-undef
+	...require("@hitbeatclub/prettier-config"),
+};
+
+// eslint-disable-next-line no-undef
+module.exports = config;
