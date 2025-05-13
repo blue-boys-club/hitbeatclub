d7bc6c30c560394e7b58ff78a6b6cfa745a0000b
정민석 <minskyson7@gmail.com>
2025-05-13 16:15:16 +0900

    fix: ArtistsDetailPage 컴포넌트 연결 변경

--- apps/web/src/app/(main)/artists/page.tsx
+++
@@ -1,6 +0,0 @@
-import ArtistProductListPage from "@/page/ArtistProductListPage";
-const ArtistTrackListRoute = () => {
-	return <ArtistProductListPage />;
-};
-
-export default ArtistTrackListRoute;
