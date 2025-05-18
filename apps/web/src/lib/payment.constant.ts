5925048c57ca130cf8c89d90f2e3031eebc1a121
blue-boys-club <blueboysclub@hitbeatclub.com>
2025-05-19 01:33:13 +0900

    fix: toss pay error
    Co-authored-by: WonJung Kim <git@won-jung.kim>

--- apps/web/src/lib/payment.constant.ts
+++ apps/web/src/lib/payment.constant.ts
@@ -14,7 +14,7 @@ export const PORTONE_CHANNEL_KEY_MAP = {
 		PAYPAL: "channel-key-9322e092-e9a0-4a8d-bf12-c349b5db5c23",
 		PAYMENT: "channel-key-f9e94e27-c30e-4134-88e7-a7beaae591d4",
 		CARD_RECURRING: "channel-key-dd409e47-d49f-459a-b379-ae1442531c6a",
-		EASY_PAY_TOSS_PAY: "channel-key-3b37819a-1c72-4deb-a245-8c810af5403d",
+		EASY_PAY_TOSS_PAY: "channel-key-2c89103f-6334-4da5-87dd-fc56360945b7",
 	},
 };
 
