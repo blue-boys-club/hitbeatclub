218ebff4f37922ccf1478c4d9d2e2778defb3953
hQQn <dahunee37@gmail.com>
2025-05-26 00:58:41 +0900

    chore

--- apps/api/src/modules/user/user.controller.ts
+++ apps/api/src/modules/user/user.controller.ts
@@ -7,8 +7,8 @@ import { DocAuth } from "src/common/doc/decorators/doc.decorator";
 import { AuthJwtAccessProtected } from "../auth/decorators/auth.jwt.decorator";
 import { createApiResponseFromZodSchema, createSwaggerSchemaFromZod } from "src/common/swagger/swagger.util";
 import { IResponse } from "src/common/response/interfaces/response.interface";
-import { UserUpdatePayloadSchema, UserUpdateResponseSchema } from "@hitbeatclub/shared-types/user";
-import { UserUpdatePayload, UserUpdateResponse } from "@hitbeatclub/shared-types/user";
+// import { UserUpdatePayloadSchema, UserUpdateResponseSchema } from "@hitbeatclub/shared-types/user";
+// import { UserUpdatePayload, UserUpdateResponse } from "@hitbeatclub/shared-types/user";
 import user from "./user.message";
 
 @Controller("user")
@@ -32,28 +32,28 @@ export class UserController {
 		return this.userService.findOne(id);
 	}
 
-	@Patch(":id")
-	@ApiOperation({ summary: "사용자 정보 수정" })
-	@DocAuth({ jwtAccessToken: true })
-	@AuthJwtAccessProtected()
-	@ApiBody({
-		description: "사용자 정보 수정 요청",
-		schema: createSwaggerSchemaFromZod(UserUpdatePayloadSchema),
-	})
-	@ApiResponse({
-		status: 200,
-		description: user.update.success,
-		schema: createApiResponseFromZodSchema(UserUpdateResponseSchema, user.update.success, 200),
-	})
-	async update(@Param("id") id: string, @Body() payload: UserUpdatePayload): Promise<IResponse<UserUpdateResponse>> {
-		// const result = await this.userService.update(id, payload);
+	// @Patch(":id")
+	// @ApiOperation({ summary: "사용자 정보 수정" })
+	// @DocAuth({ jwtAccessToken: true })
+	// @AuthJwtAccessProtected()
+	// @ApiBody({
+	// 	description: "사용자 정보 수정 요청",
+	// 	schema: createSwaggerSchemaFromZod(UserUpdatePayloadSchema),
+	// })
+	// @ApiResponse({
+	// 	status: 200,
+	// 	description: user.update.success,
+	// 	schema: createApiResponseFromZodSchema(UserUpdateResponseSchema, user.update.success, 200),
+	// })
+	// async update(@Param("id") id: string, @Body() payload: UserUpdatePayload): Promise<IResponse<UserUpdateResponse>> {
+	// 	// const result = await this.userService.update(id, payload);
 
-		return {
-			statusCode: 200,
-			message: user.update.success,
-			data: { id: parseInt(id), message: user.update.success },
-		};
-	}
+	// 	return {
+	// 		statusCode: 200,
+	// 		message: user.update.success,
+	// 		data: { id: parseInt(id), message: user.update.success },
+	// 	};
+	// }
 
 	@Delete(":id")
 	async remove(@Param("id") id: string) {
