9d0fbf24d9cbdeec576792027f1bfa8504f0f230
정민석 <48466548+phantom2115@users.noreply.github.com>
2025-06-24 13:04:46 +0900

    Feat/report api (#44)
    * feat: 신고하기 모달 api 연동
    
    * feat: 유효성 추가

--- apps/web/src/features/artist/components/ArtistReportModal.tsx
+++ apps/web/src/features/artist/components/ArtistReportModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { memo, useState } from "react";
+import { memo, MouseEvent, useState } from "react";
 import { Checkbox, ChevronRight, EmptyCheckbox } from "@/assets/svgs";
 import { Input } from "@/components/ui";
 import {
@@ -12,10 +12,12 @@ import {
 	PopupHeader,
 	PopupTitle,
 } from "@/components/ui/Popup";
+import { useReportArtistMutation } from "@/apis/artist/mutation/useReportArtistMutation";
 
 interface ArtistReportModalProps {
 	isOpen: boolean;
 	onCloseModal: () => void;
+	artistId: number;
 }
 
 interface ReportFormData {
@@ -32,15 +34,68 @@ interface ReportFormData {
  * - 개인정보 수집 동의 체크박스
  * - 전송 시 모달 닫힘과 함께 완료 토스트 메시지 표시
  */
-export const ArtistReportModal = memo(({ isOpen, onCloseModal }: ArtistReportModalProps) => {
-	const [agreement, setAgreement] = useState(false);
+export const ArtistReportModal = memo(({ isOpen, onCloseModal, artistId }: ArtistReportModalProps) => {
+	const [reporterName, setReporterName] = useState("");
+	const [reporterPhone, setReporterPhone] = useState("");
+	const [reporterEmail, setReporterEmail] = useState("");
+	const [content, setContent] = useState("");
+	const [agreedPrivacyPolicy, setAgreedPrivacyPolicy] = useState(false);
+	const [validate, setValidate] = useState({ error: false, message: "" });
+	const { mutate: reportArtist, isError, isPending } = useReportArtistMutation();
 
 	const onCheckboxChange = () => {
-		setAgreement(!agreement);
+		setAgreedPrivacyPolicy(!agreedPrivacyPolicy);
 	};
 
-	const onSendReport = () => {
-		onCloseModal();
+	const onSendReport = (e: MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+		if (!reporterName) {
+			setValidate({ error: true, message: "이름을 입력해주세요." });
+			return;
+		}
+		if (!reporterPhone) {
+			setValidate({ error: true, message: "번호를 입력해주세요." });
+			return;
+		}
+		if (!reporterEmail) {
+			setValidate({ error: true, message: "이메일을 입력해주세요." });
+			return;
+		}
+		if (!emailRegex.test(reporterEmail)) {
+			setValidate({ error: true, message: "이메일 형식을 정확히 입력해주세요." });
+			return;
+		}
+		if (!content) {
+			setValidate({ error: true, message: "신고 내용을 입력해주세요." });
+			return;
+		}
+		if (content.length < 10) {
+			setValidate({ error: true, message: "신고 내용은 10자 이상 입력해 주세요." });
+			return;
+		}
+		if (!agreedPrivacyPolicy) {
+			setValidate({ error: true, message: "수집 및 동의를 체크해주세요" });
+			return;
+		}
+
+		reportArtist(
+			{
+				id: artistId,
+				payload: { reporterName, reporterPhone, reporterEmail, content, agreedPrivacyPolicy },
+			},
+			{
+				onSuccess: () => {
+					setReporterName("");
+					setReporterPhone("");
+					setReporterEmail("");
+					setContent("");
+					setAgreedPrivacyPolicy(false);
+					onCloseModal();
+				},
+			},
+		);
 	};
 
 	return (
@@ -50,49 +105,79 @@ export const ArtistReportModal = memo(({ isOpen, onCloseModal }: ArtistReportMod
 		>
 			<PopupContent>
 				<PopupHeader>
-					<PopupTitle className="text-[26px] font-bold">신고하기</PopupTitle>
+					<PopupTitle className="text-[36px] font-normal text-black text-center font-['Actor'] leading-[32px] tracking-[0.36px]">
+						신고하기
+					</PopupTitle>
 				</PopupHeader>
 
 				<PopupDescription>
 					<form>
 						<div className="flex flex-col gap-2 mb-6">
 							<div className="flex items-center justify-between gap-2">
-								<label htmlFor="name">이름</label>
+								<label
+									htmlFor="name"
+									className="text-[#000] font-['SUIT'] text-[18px] font-semibold leading-[160%] tracking-[0.18px]"
+								>
+									이름
+								</label>
 								<Input
 									type="text"
 									id="name"
-									className="w-[300px]"
+									className={"w-[300px]"}
 									required
+									value={reporterName}
+									onChange={(e) => setReporterName(e.target.value)}
 								/>
 							</div>
 
 							<div className="flex items-center justify-between gap-2">
-								<label htmlFor="phone">휴대폰 번호</label>
+								<label
+									htmlFor="phone"
+									className="text-[#000] font-['SUIT'] text-[18px] font-semibold leading-[160%] tracking-[0.18px]"
+								>
+									휴대폰 번호
+								</label>
 								<Input
 									type="tel"
 									id="phone"
 									className="w-[300px]"
 									required
+									value={reporterPhone}
+									onChange={(e) => setReporterPhone(e.target.value)}
 								/>
 							</div>
 
 							<div className="flex items-center justify-between gap-2">
-								<label htmlFor="email">이메일 주소</label>
+								<label
+									htmlFor="email"
+									className="text-[#000] font-['SUIT'] text-[18px] font-semibold leading-[160%] tracking-[0.18px]"
+								>
+									이메일 주소
+								</label>
 								<Input
 									type="email"
 									id="email"
 									className="w-[300px]"
 									required
+									value={reporterEmail}
+									onChange={(e) => setReporterEmail(e.target.value)}
 								/>
 							</div>
 						</div>
 
 						<div className="flex flex-col gap-2 mb-6">
-							<label htmlFor="report-reason">신고 내용</label>
+							<label
+								htmlFor="report-reason"
+								className="text-[#000] font-['SUIT'] text-[18px] font-semibold leading-[160%] tracking-[0.18px]"
+							>
+								신고 내용
+							</label>
 							<textarea
 								id="report-reason"
 								className="w-full h-[100px] p-[5px] border-x-[1px] border-y-[2px] rounded-[5px]"
 								required
+								value={content}
+								onChange={(e) => setContent(e.target.value)}
 							/>
 						</div>
 
@@ -100,17 +185,27 @@ export const ArtistReportModal = memo(({ isOpen, onCloseModal }: ArtistReportMod
 							className="inline-flex items-center gap-2 mb-6 cursor-pointer"
 							onClick={onCheckboxChange}
 						>
-							{agreement ? <Checkbox /> : <EmptyCheckbox />}
-							<span className="inline-flex items-center gap-2">
+							{agreedPrivacyPolicy ? <Checkbox /> : <EmptyCheckbox />}
+							<span className="inline-flex items-center gap-2 text-[#000] font-['SUIT'] text-[12px] font-semibold leading-[150%] tracking-[0.12px]">
 								[필수] 개인정보 수집 및 이용에 동의합니다.
 								<ChevronRight />
 							</span>
 						</div>
+						{validate.error && (
+							<div className="items-center gap-2 font-['SUIT'] text-[12px] font-semibold leading-[150%] tracking-[0.12px] text-hbc-red">
+								{validate.message}
+							</div>
+						)}
+						{isError && (
+							<div className="inline-flex items-center gap-2 font-['SUIT'] text-[12px] font-semibold leading-[150%] tracking-[0.12px] text-hbc-red">
+								신고 전송 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.
+							</div>
+						)}
 					</form>
 				</PopupDescription>
 
 				<PopupFooter>
-					<PopupButton onClick={onSendReport}>전송하기</PopupButton>
+					<PopupButton onClick={onSendReport}>{isPending ? "전송중..." : "전송하기"}</PopupButton>
 				</PopupFooter>
 			</PopupContent>
 		</Popup>
