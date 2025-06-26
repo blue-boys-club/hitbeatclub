3c3d24350370145524678d9a5e1cd5369959c429
hQQn <dahunee37@gmail.com>
2025-06-26 21:15:18 +0900

    fix: ArtistCreateSchema

--- packages/shared-types/src/artist/artist.request.ts
+++ packages/shared-types/src/artist/artist.request.ts
@@ -6,19 +6,19 @@ export const ArtistCreateSchema = z.object({
 	slug: z.string().max(20).describe("슬러그").default("dj-cool").optional(),
 	description: z.string().max(1000).describe("아티스트 설명").default("안녕하세요! DJ Cool입니다.").optional(),
 	profileImageFileId: z.number().describe("프로필 이미지 파일 ID").optional(),
-	instagramAccount: z.string().max(30).describe("인스타그램 계정").default("djcool").optional(),
-	youtubeAccount: z.string().max(30).describe("유튜브 계정").default("djcool").optional(),
-	tiktokAccount: z.string().max(30).describe("틱톡 계정").default("djcool").optional(),
-	soundcloudAccount: z.string().max(30).describe("사운드클라우드 계정").default("djcool").optional(),
+	instagramAccount: z.string().max(30).describe("인스타그램 계정").default("djcool").nullable().optional(),
+	youtubeAccount: z.string().max(30).describe("유튜브 계정").default("djcool").nullable().optional(),
+	tiktokAccount: z.string().max(30).describe("틱톡 계정").default("djcool").nullable().optional(),
+	soundcloudAccount: z.string().max(30).describe("사운드클라우드 계정").default("djcool").nullable().optional(),
 	etcAccounts: z
 		.array(z.string())
 		.nullable()
 		.describe("기타 계정들")
 		.default(["https://twitter.com/djcool"])
 		.optional(),
-	kakaoAccount: z.string().max(30).describe("카카오 계정").default("djcool").optional(),
-	lineAccount: z.string().max(30).describe("라인 계정").default("djcool").optional(),
-	discordAccount: z.string().max(30).describe("디스코드 계정").default("djcool#1234").optional(),
+	kakaoAccount: z.string().max(30).describe("카카오 계정").default("djcool").nullable().optional(),
+	lineAccount: z.string().max(30).describe("라인 계정").default("djcool").nullable().optional(),
+	discordAccount: z.string().max(30).describe("디스코드 계정").default("djcool#1234").nullable().optional(),
 	country: z.string().max(3).describe("국가").default("KOR").optional(),
 	city: z.string().max(100).describe("도시").default("서울").optional(),
 });
